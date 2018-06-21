a1087b0deb70831cbbd709d8f7e1bcc252be02bd
yuriirogulya <yuriirogulya@gmail.com>
2018-06-21 12:54:21 +0300

    added possibility to update your own profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: %i[show edit update destroy]
+  before_action :find_user, only: %i[index show edit update destroy]
   before_action :authenticate_user!
 
   def index
@@ -7,7 +7,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users, skip: [:passwords]
 
-  get 'profile', to: 'accounts#index'
+  get 'accounts', to: 'accounts#index'
+
   resources :users, except: [:destroy] do
-    resources :accounts do
+    resources :accounts, except: [:show, :edit , :update] do
       post 'transaction', to: 'accounts#transaction'
     end
   end
