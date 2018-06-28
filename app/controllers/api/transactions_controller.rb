d82bbe7740adb26d65b7e932424a689093a8fd2c
yuriirogulya <yuriirogulya@gmail.com>
2018-06-29 01:18:41 +0300

    added transaction history API controller

---
+++ app/controllers/api/transactions_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  class TransactionsController < BaseController
+    before_action :find_transaction, only: %i[destroy]
+    has_scope :by_account_id, as: :account
+    has_scope :by_user_id, as: :user
+
+    def index
+      @transactions = apply_scopes(Transaction).all
+      render json: { transactions: @transactions }
+    end
+
+    def destroy
+      if @transaction.destroy
+        render json: { msg: 'Transaction was deleted' }
+      else
+        render json: { errors: @transaction.errors.full_messages }, status: :unprocessible_entity
+      end
+    end
+
+    private
+
+    def find_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+  end
+end
